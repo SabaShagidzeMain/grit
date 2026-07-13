6bbfb03313683b5770e5248a443278a7c1f67e6d
Saba Shagidze <sabadoeswebdev@gmail.com>
2026-07-13 22:42:54 +0400

    feat: wsgi fo pythonanywhere

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+# Add your project directory to the sys.path
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Import your Flask app
+from app import create_app
+
+application = create_app()
